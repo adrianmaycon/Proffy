d720a5dcc92985254d55f3486a66cb8521d9560b
adrianmaycon <mayconlemos1010@gmail.com>
2020-08-13 11:27:30 -0300

    subindo projeto

---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// GET:Buscar ou listar uma informação
+// POST: Criar alguma nova informação
+// PUT: Atualizar uma informação existente
+// DELETE: Deletar uma informação existente
+
+// Corpo (Resquest Body): Dados para criação ou atualização de um registro
+// Route Params: Identificar qual recurso eu quero atualziar ou deletar
+// Query Params: Paginação, filtros, ordenação
+
+app.use(routes);
+
+app.listen(3333);
+
